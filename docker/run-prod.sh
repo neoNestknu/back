d8f996dbe968f18d682691133fee48763182be5e
Observersss <heavyobjectics@gmail.com>
2025-12-02 07:41:45 +0200

    fix: some fixes 2.0

--- docker/run-prod.sh
+++ docker/run-prod.sh
@@ -7,10 +7,11 @@ startMsg
 
 copyEnvs
 
-docker build -t nnk:latest ../auth/auth
-docker build -t nnk:latest ../migration_module
-docker build -t nnk:latest ../notification
-docker build -t nnk:latest ../migration_module
+docker build -t nnk-auth:latest ../auth
+docker build -t nnk-migration:latest ../migration_module
+docker build -t nnk-notification:latest ../notification
+docker build -t nnk-user:latest ../user
+docker build -t nnk-auction:latest ../auction
 
 docker compose -f docker-compose.prod.yml up -d
 
--- docker/sub-scripts/env.sh
+++ docker/sub-scripts/env.sh
@@ -7,4 +7,5 @@ copyEnvs() {
   cp ../docs/example.env ../docs/.env
   cp ../migration_module/example.env ../migration_module/.env
   cp ../minio-local/example.env ../minio-local/.env
+  cp ../user/example.env ../user/.env
 }
